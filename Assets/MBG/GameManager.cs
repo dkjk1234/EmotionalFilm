d593a1227ce7b92d08e322a41f7144d22d64639e
mingi <107851917+tigerspower@users.noreply.github.com>
2023-07-10 18:10:40 +0900

    tuning

--- Assets/MBG/GameManager.cs
+++ Assets/MBG/GameManager.cs
@@ -5,11 +5,9 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     private static GameManager _instance;
-    // 인스턴스에 접근하기 위한 프로퍼티
     public static GameManager Instance
     {
         get {
-            // 인스턴스가 없는 경우에 접근하려 하면 인스턴스를 할당해준다.
             if(!_instance)
             {
                 _instance = FindObjectOfType(typeof(GameManager)) as GameManager;
@@ -27,12 +25,10 @@ public class GameManager : MonoBehaviour
         {
             _instance = this;
         }
-        // 인스턴스가 존재하는 경우 새로생기는 인스턴스를 삭제한다.
         else if (_instance != this)
         {
             Destroy(gameObject);
         }
-        // 아래의 함수를 사용하여 씬이 전환되더라도 선언되었던 인스턴스가 파괴되지 않는다.
         DontDestroyOnLoad(gameObject);
     }
     //Start is called before the first frame update
