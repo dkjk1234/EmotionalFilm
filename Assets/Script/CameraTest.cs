2f22e5035db413394a0fd142862015102665e187
suji_sz <103244178+Yoonsuji@users.noreply.github.com>
2023-07-11 18:49:51 +0900

    v키 누르면 고정 카메라, 시야가리기 공격 수정, 파티클 콜라이더 넣기

--- Assets/Script/CameraTest.cs
+++ Assets/Script/CameraTest.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraTest : MonoBehaviour
 {
-    public gameObject LookAt;
+    public CinemachineFreeLook thirdPersonCamera;
+    public CinemachineVirtualCamera fixCamera;
+
+    private bool isfixMode = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +18,12 @@ public class CameraTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.V))
+        {
+            isfixMode = !isfixMode;
+
+            thirdPersonCamera.gameObject.SetActive(!isfixMode);
+            fixCamera.gameObject.SetActive(!isfixMode);
+        }
     }
 }
--- Assets/Script/bullet.cs
+++ Assets/Script/bullet.cs
@@ -1,15 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class bullet : MonoBehaviour
 {
     public float speed = 10f;
- 
+    public Image image;
+    public float fadeDuration = 1f;
+    private ParticleSystem particleSystem;
+    private ParticleCollisionEvent[] collisionEvents;
+    public bool isCollided = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        particleSystem = GetComponent<ParticleSystem>();
+        collisionEvents = new ParticleCollisionEvent[16];
+        image.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+    }
+
+
+    private void OnParticleCollision(GameObject other)
+    {
+        int numCollisions = particleSystem.GetCollisionEvents(other, collisionEvents);
+        for (int i = 0; i < numCollisions; i++)
+        {
+            Vector3 collisionPosition = collisionEvents[i].intersection;
+            Vector3 collisionNormal = collisionEvents[i].normal;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(FadeInAndOutImage());
+            if(!isCollided)
+            {
+                isCollided = true;
+            }
+        }
+    }
+
+    private IEnumerator FadeInAndOutImage()
+    {
+        yield return StartCoroutine(FadeIn());
+        yield return new WaitForSeconds(fadeDuration);
+        yield return StartCoroutine(FadeOut());
+    }
+
+    private IEnumerator FadeIn()
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < fadeDuration)
+        {
+            float alpha = Mathf.Lerp(0f, 1f, elapsedTime / fadeDuration);
+            image.color = new Color(image.color.r, image.color.g, image.color.b, alpha);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        image.color = new Color(image.color.r, image.color.g, image.color.b, 1f);
+
+    }
+
+    private IEnumerator FadeOut()
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < fadeDuration)
+        {
+            float alpha = Mathf.Lerp(1f, 0f, elapsedTime / fadeDuration);
+            image.color = new Color(image.color.r, image.color.g, image.color.b, alpha);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        image.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
     }
 
     // Update is called once per frame
