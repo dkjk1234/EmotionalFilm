afbefd2d0c8f6d4eda2125167b6468d6c3f264f0
mingi <107851917+tigerspower@users.noreply.github.com>
2023-07-13 11:45:06 +0900

    Portal Update

--- Assets/MBG/MapControl.cs
+++ Assets/MBG/MapControl.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MonsterControl : MonoBehaviour
+public class MapControl : MonoBehaviour
 {
     public GameObject MonsterPos0;
     public GameObject MonsterPos1;
@@ -14,21 +14,33 @@ public class MonsterControl : MonoBehaviour
 
     public GameObject Door;
 
+    public GameObject Target;
+
     bool MonsterTag = true;
+
+    bool BossTag = true;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        Target.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         MonsterTag = GameObject.FindGameObjectWithTag("Monster");
+        
         if(MonsterTag == false)
         {
             GameObject.Destroy(Door);
+        }
+
+        BossTag = GameObject.FindGameObjectWithTag("Boss");
+        
+        if(BossTag == false)
+        {
+            Target.SetActive(true);
         }
     }
     private void OnCollisionEnter(Collision collision)
