e7215a2887e11742f2c7f41c817220d3701f1e0b
suji_sz <103244178+Yoonsuji@users.noreply.github.com>
2023-07-14 18:57:52 +0900

    커스터마이징씬

---
+++ Assets/HairControll.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HairControll : MonoBehaviour
+{
+    public GameObject[] hairprefabs;
+
+    // Start is called before the first frame update
+    void OncClickExit()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
