30345b7348c19bc444b2617b7d6704fde34cade4
Lei da sorte <rlawjddn528@gmail.com>
2023-07-14 18:59:28 +0900

    Add Asset Liquid Mesh

---
+++ Assets/KJW/2.Script/BossHp.cs
@@ -0,0 +1,20 @@
+using PaintIn3D;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHp : MonoBehaviour
+{
+    public P3dChangeCounter p3DChangeCounter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameManager.Instance.uIScript.bossHealthSlider.value = 100096 - p3DChangeCounter.Count;
+    }
+}
